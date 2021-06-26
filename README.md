b868aa213d881be5c9faa5a0a477a30517d91fb6
Timothy Ji <JiTimothy@outlook.com>
2021-06-26 19:11:00 -0400

    replaced image output with json output.

--- README.md
+++ README.md
@@ -1,15 +1,47 @@
 # Genshin Impact Inventory Reader
 Playing around with Computer Vision and Genshin Impact Inventories.
-Something to highlight inventory slots and try to read the quantities from a Genshin Impact Inventory
+Something to read a Genshin Impact Inventory image and output a JSON.
 
-Uses pytesseract and opencv-python
+Uses easyocr and opencv-python
 
 #### Current Limitations:
-- Resolution can strongly affect results
-- Not always accurate
+- Resolution can affect results (built for 16:9 1080p inventory images)
+- Not always accurate, and if a material is found, but if quantity can not be read, it can return it with a `-1`.
+- Only supporting 36 materials right now, but easily extensible.
 
 ## Example:
 ### Example Input Image: 
 ![Example Input Image](https://files.timothyji.com/projects/genshin-reader/ex.png)
-### Example Output Image:
-![Example Output Image](https://files.timothyji.com/projects/genshin-reader/ex_postreaderJ21.png)
+### Example Output:
+```
+{
+  "heros-wit": 286,
+  "adventurers-experience": 90,
+  "wanderers-advice": 24,
+  "slime-concentrate": 56,
+  "slime-secretions": 205,
+  "slime-condensate": 1002,
+  "ominous-mask": 38,
+  "stained-mask": 73,
+  "damaged-mask": 796,
+  "forbidden_curse-scroll": 37,
+  "sealed-scroll": 126,
+  "divining-scroll": 719,
+  "weathered-arrowhead": 30,
+  "sharp-arrowhead": 55,
+  "firm-arrowhead": 562,
+  "black_crystal-horn": 21,
+  "black_bronze-horn": 138,
+  "heavy-horn": 733,
+  "leyline-sprout": -1,
+  "dead-leyline-leaves": 66,
+  "dead-leyline-branch": 298,
+  "chaos-core": 25,
+  "chaos-circuit": 72,
+  "chaos-device": 328,
+  "mistgrass-wick": 21,
+  "mistgrass": 80,
+  "mistgrass-pollen": 356,
+  "inspectors-sacrificial_knife": 10
+}
+```
--- src/genshinreader.py
+++ src/genshinreader.py
@@ -1,21 +1,42 @@
 import cv2 as cv
-import numpy as np
 import easyocr
 import sys
 import argparse
 import time
 import json
-import textwrap
 
 start = time.time()
 
+input_img = ''
+res = 1080
+
+parser = argparse.ArgumentParser(sys.argv)
+parser.add_argument("img", help="Image source path.", type=str)
+parser.add_argument(
+    "-r", "--res", dest="res", default=1080, help="Resolution of the game instance the image was taken from.", type=int)
+parser.add_argument(
+    "-if", "--include-failed", default=True, dest="include_failed", help="Resolution of the game instance the image was taken from.", type=bool)
+args = parser.parse_args()
+
+input_img = args.img
+res = args.res
+inc_failed = args.include_failed
+
 reader = easyocr.Reader(['en'], False)
 
+with open("reader.json") as f:
+    readerdata = json.load(f)
+with open("items\\items.json") as f:
+    itemdata = json.load(f)
+item_list = itemdata['items']
 with open("naming.json") as f:
     namingdata = json.load(f)
+
+
 def get_name(key: str) -> str:
     """Returns name of item."""
     return namingdata['map'][key]
+
 
 def find(img, look_for, threshold: float):
     """Looks for best matching <look_for> in <img> above <threshold>."""
@@ -25,53 +46,37 @@ def find(img, look_for, threshold: float):
         return max_loc
     return None
 
-def get_quantity(img):
-    number = reader.readtext(gray[bottom_right[1]-int(h*4/5):bottom_right[1], top_left[0]:bottom_right[0]], allowlist='0123456789', detail=0)
+
+def get_quantity(img) -> int:
+    number = reader.readtext(img, allowlist='0123456789', detail=0)
     if len(number) > 0:
-        return number[0]
+        return int(number[0])
     return -1
 
-def add_text_to_img(img, name, num, top_left):
-    wrapped_name = textwrap.wrap(name + ' ' + str(num), width=14)
 
-    font = cv.FONT_HERSHEY_SIMPLEX
-    font_size = 0.5
-    font_thickness = 1
-    for i, line in enumerate(wrapped_name):
-        textsize = cv.getTextSize(line, font, font_size, font_thickness)[0]
+def find_matches(item_list, include_failed):
+    matched = {}
+    for i, item in enumerate(item_list):
+        name = get_name(item)
+        print(f'{int((i+1)/len(item_list)*100)}% | {i+1}/{len(item_list)}')
+        template = cv.imread("items\\" + item, cv.IMREAD_GRAYSCALE)
+        match = find(gray, template, 0.9)
+        if match:
+            h, w = template.shape
+            top_left = match
+            bottom_right = (top_left[0] + w, top_left[1] + h)
+            quantity_segment = gray[bottom_right[1] -
+                                    int(h*4/5):bottom_right[1], top_left[0]:bottom_right[0]]
+            quantity = get_quantity(quantity_segment)
+            if quantity != -1 or include_failed:
+                matched[name] = quantity
+    return matched
 
-        gap = textsize[1] + 10
-
-        y = int((top_left[1] + textsize[1])) + i * gap
-        x = int((top_left[0]))
-        cv.putText(img, line, (x, y), font,
-                    font_size, 
-                    (0,0,0), 
-                    font_thickness, 
-                    lineType = cv.LINE_AA)
-
-input_img = ''
-res = 1080
-
-parser = argparse.ArgumentParser(sys.argv)
-parser.add_argument("img", help="Image source path.", type=str)
-parser.add_argument(
-    "res", help="Resolution of the game instance the image was taken from.", type=int)
-args = parser.parse_args()
-
-input_img = args.img
-res = args.res
-
-with open("reader.json") as f:
-    readerdata = json.load(f)
-with open("items\\items.json") as f:
-    itemdata = json.load(f)
 
 print("Engine:Version=" + readerdata['version'])
 print("Item List:Version=" + itemdata['version'])
 print("Naming Scheme:Version=" + namingdata['scheme'])
 
-item_list = itemdata['items']
 img = cv.imread(input_img)
 
 scale = 1080/res
@@ -79,26 +84,9 @@ scale = 1080/res
 img = cv.resize(img, (int(img.shape[1]*scale), int(img.shape[0]*scale)))
 gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 
-for item in item_list:
-    name = get_name(item)
-    print(name)
-
-    template = cv.imread("items\\" + item, cv.IMREAD_GRAYSCALE)
-    
-    match = find(gray, template, 0.9)
-    if match:
-        h, w = template.shape
-        top_left = match
-        bottom_right = (top_left[0] + w, top_left[1] + h)
-        
-        quantity_segment = gray[bottom_right[1]-int(h*4/5):bottom_right[1], top_left[0]:bottom_right[0]]
-
-        num = get_quantity(quantity_segment)
-
-        add_text_to_img(img, name, num, top_left)
+matches = find_matches(item_list, inc_failed)
 
 end = time.time()
-print(end-start)
+print("Time Elapsed (s):", end-start)
 
-cv.imshow('Annotated', img)
-cv.waitKey(0)
+print(matches)
