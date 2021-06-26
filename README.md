d3856058e3d58f4e0031581b85c2a05af61b0d22
Timothy Ji <39247801+Timothy-Ji@users.noreply.github.com>
2021-06-26 19:54:38 -0400

    Revamped Engine and JSON Output (#1)
    Removed Image Output and replaced it with a python dictionary output.
    Changed OCR from Tesseract to EasyOCR.
    Revamped how materials are identified. (No longer need to know particular rows and columns, but need to know resolution)
    Currently Support determining of 36 different materials.

--- README.md
+++ README.md
@@ -1,16 +1,75 @@
 # Genshin Impact Inventory Reader
 Playing around with Computer Vision and Genshin Impact Inventories.
-Something to highlight inventory slots from a Genshin Impact Inventory
+Something to read a Genshin Impact Inventory image and output a JSON.
 
-Uses pytesseract and opencv-python
+Uses `easyocr` and `opencv-python`
 
 #### Current Limitations:
-- Requires the number of columns and rows to be known
-- Resolution can affect results
-- Not always accurate
+- Resolution can affect results (built for 16:9 1080p inventory images)
+- Not always accurate, and if a material is found, but if quantity can not be read, it can return it with a `-1`.
+- Only supporting 36 materials right now, but easily extensible.
+
+## Usage:
+`python3 genshinreader.py <image_path>`
+### Optional arguments:
+#### Resolution:
+- Resolution of image items relative to actual game resolution.
+- Usage:  `-r`/`--res` `<resolution>`
+- Default: `1080`
+#### Include Failed:
+- Whether to include `material: quantity` where the quantity could not be read.
+- Usage: `-if`/`--include-failed` `<include_failed>`
+- Default: `True`
+
+## What are all the version numbers?:
+Engine Version: `name:major.minor.revision`
+- Version of the image reading system for Genshin Reader.
+
+List Version: `name:major.minor.revision`
+- Version of list of images to pull from for template matching.
+
+Naming Scheme: `scheme:[version]`
+- Version: `major.minor.revision`
+- Naming scheme and version of output names for materials.
 
 ## Example:
 ### Example Input Image: 
 ![Example Input Image](https://files.timothyji.com/projects/genshin-reader/ex.png)
-### Example Output Image:
-![Example Output Image](https://files.timothyji.com/projects/genshin-reader/ex_postreaderECF0.png)
+### Example Output:
+```
+{
+  "engine-version": "genshinreader:1.0.0",
+  "list-version": "genshinreader:0.2.0",
+  "naming-scheme": "default:0.2.0",
+  "materials": {
+    "heros-wit": 286,
+    "adventurers-experience": 90,
+    "wanderers-advice": 24,
+    "slime-concentrate": 56,
+    "slime-secretions": 205,
+    "slime-condensate": 1002,
+    "ominous-mask": 38,
+    "stained-mask": 73,
+    "damaged-mask": 796,
+    "forbidden_curse-scroll": 37,
+    "sealed-scroll": 126,
+    "divining-scroll": 719,
+    "weathered-arrowhead": 30,
+    "sharp-arrowhead": 55,
+    "firm-arrowhead": 562,
+    "black_crystal-horn": 21,
+    "black_bronze-horn": 138,
+    "heavy-horn": 733,
+    "leyline-sprout": -1,
+    "dead-leyline-leaves": 66,
+    "dead-leyline-branch": 298,
+    "chaos-core": 25,
+    "chaos-circuit": 72,
+    "chaos-device": 328,
+    "mistgrass-wick": 21,
+    "mistgrass": 80,
+    "mistgrass-pollen": 356,
+    "inspectors-sacrificial_knife": 10
+  }
+}
+```
---
+++ src/genshinreader.py
@@ -0,0 +1,94 @@
+import cv2 as cv
+import easyocr
+import sys
+import argparse
+import time
+import json
+
+start = time.time()
+
+input_img = ''
+res = 1080
+
+parser = argparse.ArgumentParser(sys.argv)
+parser.add_argument("img", help="Image source path.", type=str)
+parser.add_argument(
+    "-r", "--res", dest="res", default=1080, help="Resolution of the game instance the image was taken from.", type=int)
+parser.add_argument(
+    "-if", "--include-failed", default=True, dest="include_failed", help="Resolution of the game instance the image was taken from.", type=bool)
+args = parser.parse_args()
+
+input_img = args.img
+res = args.res
+inc_failed = args.include_failed
+
+reader = easyocr.Reader(['en'], False)
+
+with open("reader.json") as f:
+    readerdata = json.load(f)
+with open("items\\items.json") as f:
+    itemdata = json.load(f)
+item_list = itemdata['items']
+with open("naming.json") as f:
+    namingdata = json.load(f)
+
+
+def get_name(key: str) -> str:
+    """Returns name of item."""
+    return namingdata['map'][key]
+
+
+def find(img, look_for, threshold: float):
+    """Looks for best matching <look_for> in <img> above <threshold>."""
+    result = cv.matchTemplate(img, look_for, cv.TM_CCOEFF_NORMED)
+    _, max_val, _, max_loc = cv.minMaxLoc(result)
+    if max_val >= threshold:
+        return max_loc
+    return None
+
+
+def get_quantity(img) -> int:
+    number = reader.readtext(img, allowlist='0123456789', detail=0)
+    if len(number) > 0:
+        return int(number[0])
+    return -1
+
+
+def find_matches(item_list, include_failed):
+    matched = {}
+    for i, item in enumerate(item_list):
+        name = get_name(item)
+        print(f'{int((i+1)/len(item_list)*100)}% | {i+1}/{len(item_list)}')
+        template = cv.imread("items\\" + item, cv.IMREAD_GRAYSCALE)
+        match = find(gray, template, 0.9)
+        if match:
+            h, w = template.shape
+            top_left = match
+            bottom_right = (top_left[0] + w, top_left[1] + h)
+            quantity_segment = gray[bottom_right[1] -
+                                    int(h*4/5):bottom_right[1], top_left[0]:bottom_right[0]]
+            quantity = get_quantity(quantity_segment)
+            if quantity != -1 or include_failed:
+                matched[name] = quantity
+    return matched
+
+
+print("Engine Version=" + readerdata['version'])
+print("List Version=" + itemdata['version'])
+print("Naming Scheme=" + namingdata['scheme'])
+
+img = cv.imread(input_img)
+
+scale = 1080/res
+
+img = cv.resize(img, (int(img.shape[1]*scale), int(img.shape[0]*scale)))
+gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+
+matches = find_matches(item_list, inc_failed)
+
+end = time.time()
+print("Time Elapsed (s):", end-start)
+
+output = {"engine-version": readerdata['version'], "list-version": itemdata['version'], "naming-scheme": namingdata['scheme'], "materials": {} }
+output['materials'] = matches
+print(output)
