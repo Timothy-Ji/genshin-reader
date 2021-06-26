bf8e8d1be235554ce250f8f0362316f0c1a44836
Timothy Ji <JiTimothy@outlook.com>
2021-06-26 18:10:33 -0400

    split some functionality into their own functions.

--- src/genshinreader.py
+++ src/genshinreader.py
@@ -11,6 +11,45 @@ start = time.time()
 
 reader = easyocr.Reader(['en'], False)
 
+with open("naming.json") as f:
+    namingdata = json.load(f)
+def get_name(key: str) -> str:
+    """Returns name of item."""
+    return namingdata['map'][key]
+
+def find(img, look_for, threshold: float):
+    """Looks for best matching <look_for> in <img> above <threshold>."""
+    result = cv.matchTemplate(img, look_for, cv.TM_CCOEFF_NORMED)
+    _, max_val, _, max_loc = cv.minMaxLoc(result)
+    if max_val >= threshold:
+        return max_loc
+    return None
+
+def get_quantity(img):
+    number = reader.readtext(gray[bottom_right[1]-int(h*4/5):bottom_right[1], top_left[0]:bottom_right[0]], allowlist='0123456789', detail=0)
+    if len(number) > 0:
+        return number[0]
+    return -1
+
+def add_text_to_img(img, name, num, top_left):
+    wrapped_name = textwrap.wrap(name + ' ' + str(num), width=14)
+
+    font = cv.FONT_HERSHEY_SIMPLEX
+    font_size = 0.5
+    font_thickness = 1
+    for i, line in enumerate(wrapped_name):
+        textsize = cv.getTextSize(line, font, font_size, font_thickness)[0]
+
+        gap = textsize[1] + 10
+
+        y = int((top_left[1] + textsize[1])) + i * gap
+        x = int((top_left[0]))
+        cv.putText(img, line, (x, y), font,
+                    font_size, 
+                    (0,0,0), 
+                    font_thickness, 
+                    lineType = cv.LINE_AA)
+
 input_img = ''
 res = 1080
 
@@ -27,8 +66,6 @@ with open("reader.json") as f:
     readerdata = json.load(f)
 with open("items\\items.json") as f:
     itemdata = json.load(f)
-with open("naming.json") as f:
-    namingdata = json.load(f)
 
 print("Engine:Version=" + readerdata['version'])
 print("Item List:Version=" + itemdata['version'])
@@ -38,53 +75,27 @@ item_list = itemdata['items']
 img = cv.imread(input_img)
 
 scale = 1080/res
-img = cv.resize(img, (int(img.shape[1]*scale), int(img.shape[0]*scale)))
 
+img = cv.resize(img, (int(img.shape[1]*scale), int(img.shape[0]*scale)))
 gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 
-templates = {}
 for item in item_list:
-    templates[item] = cv.imread("items\\" + item, cv.IMREAD_GRAYSCALE)
-
-for item in item_list:
-    name = namingdata[item]
+    name = get_name(item)
     print(name)
-    template = cv.imread("items\\" + item)
-    grayTemplate = cv.cvtColor(template, cv.COLOR_BGR2GRAY)
-    result = cv.matchTemplate(gray, grayTemplate, cv.TM_CCOEFF_NORMED)
-    min_val, max_val, min_loc, max_loc = cv.minMaxLoc(result)
-    threshold = 0.9
-    loc = np.where(result >= threshold)
-    print('Best match top left position: %s' % str(max_loc))
-    print('Best match confidence: %s' % max_val)
 
-    h, w, _ = template.shape
-
-    if max_val >= threshold:
-        top_left = max_loc
+    template = cv.imread("items\\" + item, cv.IMREAD_GRAYSCALE)
+    
+    match = find(gray, template, 0.9)
+    if match:
+        h, w = template.shape
+        top_left = match
         bottom_right = (top_left[0] + w, top_left[1] + h)
         
-        numberl = reader.readtext(gray[bottom_right[1]-int(h*4/5):bottom_right[1], top_left[0]:bottom_right[0]], allowlist='0123456789', detail=0)
-        num = -1
-        if (len(numberl) > 0):
-            num = numberl[0]
+        quantity_segment = gray[bottom_right[1]-int(h*4/5):bottom_right[1], top_left[0]:bottom_right[0]]
 
-        wrapped_name = textwrap.wrap(name + ' ' + str(num), width=14)
-        font = cv.FONT_HERSHEY_SIMPLEX
-        font_size = 0.5
-        font_thickness = 1
-        for i, line in enumerate(wrapped_name):
-            textsize = cv.getTextSize(line, font, font_size, font_thickness)[0]
+        num = get_quantity(quantity_segment)
 
-            gap = textsize[1] + 10
-
-            y = int((top_left[1] + textsize[1])) + i * gap
-            x = int((top_left[0]))
-            cv.putText(img, line, (x, y), font,
-                        font_size, 
-                        (0,0,0), 
-                        font_thickness, 
-                        lineType = cv.LINE_AA)
+        add_text_to_img(img, name, num, top_left)
 
 end = time.time()
 print(end-start)
